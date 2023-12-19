98c570eba7f6f9e812557c32703185aa2a60b2f6
Bill Stark <billstark001@gmail.com>
2023-12-19 13:42:59 +0900

    optimize random algorithm

--- recsys/random.py
+++ recsys/random.py
@@ -19,19 +19,13 @@ class Random(HKModelRecommendationSystem):
   def recommend(self, agent: HKAgent, neighbors: List[HKAgent], rate: float) -> List[HKAgent]:
     exclude_ids = np.array([x.unique_id for x in neighbors])
     required_count = int(rate * len(neighbors) + 0.5)
-    ret = np.zeros((0, ), dtype=int)
-    for _ in range(10):
-      candidates = np.random.randint(0, self.num_nodes, (required_count - ret.size, ))
-      applied = np.setdiff1d(candidates, exclude_ids)
-      exclude_ids = np.concatenate([exclude_ids, applied])
-      ret = np.concatenate([ret, applied])
-      if len(ret) >= required_count:
-        ret = ret[:required_count]
-        break
-    return [self.agent_map[a] for a in ret]
+    candidates = np.random.randint(
+        0, self.num_nodes, (required_count + len(neighbors), ))
+    ret = np.setdiff1d(candidates, exclude_ids)
+    return [self.agent_map[a] for a in ret[:required_count]]
 
   def post_step(self, changed: List[int]):
     pass
-  
+
   def pre_commit(self):
     pass
