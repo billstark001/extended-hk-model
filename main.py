1fc807e81c32da91352dd0d38d613b325dc7b869
Bill Stark <billstark001@gmail.com>
2023-12-28 16:58:24 +0900

    add data collector

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from model import HKModel, HKModelParams
 from scenario import ScenarioParams, Scenario
 from recsys import Random, Opinion, Structure
 
 s_params = ScenarioParams(
-    n_agent=1000,
-    n_edges=15,
-    n_step=300,
+    agent_count=1000,
+    agent_follow=15,
+    total_step=500,
+    stat_interval=15,
 )
 
 params = HKModelParams(
@@ -16,12 +18,17 @@ params = HKModelParams(
     decay=0.1,
     rewiring_rate=0.03,
     recsys_count=10,
-    recsys_factory=Random,
+    recsys_factory=Structure,
 )
 
 S = Scenario(s_params, params)
 S.init()
 S.step()
+
+# plot
+
+sns.set()
+
 opinion, dn, dr = S.get_opinion_data()
 # (t, n)
 
@@ -34,15 +41,36 @@ mn = np.mean(dn, axis=1)
 sn = np.std(dn, axis=1)
 mr = np.mean(dr, axis=1)
 sr = np.std(dr, axis=1)
+
 plt.plot(sn, lw=1)
 plt.plot(sr, lw=1)
 plt.plot(sn + sr, lw=1)
 plt.legend(['Neighbor', 'Recommended', 'Total'])
 plt.title('Standard Deviation')
 plt.show()
-plt.plot(mn, lw=1)
-plt.plot(mr, lw=1)
-plt.plot(mn + mr, lw=1)
-plt.legend(['Neighbor', 'Recommended', 'Total'])
-plt.title('Mean')
+
+# plt.plot(mn, lw=1)
+# plt.plot(mr, lw=1)
+# plt.plot(mn + mr, lw=1)
+# plt.legend(['Neighbor', 'Recommended', 'Total'])
+# plt.title('Mean')
+# plt.show()
+
+stats = S.stats
+stats_index = sorted(stats.keys())
+distance, triads, clustering, segregation = [
+    [stats[i][n] for i in stats_index] \
+    for n in range(4)
+]
+
+plt.plot(stats_index, triads)
+plt.title('Count of Closed Triads')
+plt.show()
+
+plt.plot(stats_index, clustering)
+plt.title('Average Clustering Coefficient')
+plt.show()
+
+plt.plot(stats_index, segregation)
+plt.title('Segregation Index')
 plt.show()
--- scenario.py
+++ scenario.py
@@ -1,4 +1,5 @@
-from typing import Dict, Tuple, List, Optional
+from typing import Dict, Tuple, List, Optional, Any
+from numpy.typing import NDArray
 
 import dataclasses
 
@@ -13,24 +14,30 @@ from tqdm import tqdm
 
 @dataclasses.dataclass
 class ScenarioParams:
-  n_agent: int = 1000
-  n_edges: int = 20
-  n_step: int = 1000
+  agent_count: int = 1000
+  agent_follow: int = 20
+  total_step: int = 1000
+  data_interval: int = 1
+  stat_interval: int = 20
   opinion_range: Tuple[int, int] = (-1, 1)
 
   def generate(self):
     graph: nx.DiGraph = nx.erdos_renyi_graph(
-        n=self.n_agent,
-        p=self.n_edges / (self.n_agent - 1),
+        n=self.agent_count,
+        p=self.agent_follow / (self.agent_count - 1),
         directed=True
     )
-    opinion = np.random.uniform(*self.opinion_range, (self.n_agent, ))
+    opinion = np.random.uniform(*self.opinion_range, (self.agent_count, ))
     return graph, opinion
+
+StatsType = Tuple[Tuple[NDArray[Any], NDArray[Any]], int, float, float]
 
 class Scenario:
 
   model: HKModel = None
   datacollector: DataCollector = None
+  stats: Dict[int, StatsType] = None
+  steps: int = 0
 
   def __init__(
       self,
@@ -39,6 +46,8 @@ class Scenario:
   ):
     self.scenario_params = scenario_params
     self.model_params = model_params
+    self.stats = {}
+    self.steps = 0
     
   def init_data(self, collect=True):
     self.datacollector = DataCollector(agent_reporters=dict(
@@ -46,20 +55,23 @@ class Scenario:
       DiffNeighbor='diff_neighbor',
       DiffRecommended='diff_recommended'
     ))
+    self.stats = {}
     if collect:
       self.datacollector.collect(self.model)
+      self.add_stats()
     self.model.datacollector = self.datacollector
 
   def init(self):
     graph, opinion = self.scenario_params.generate()
     model = HKModel(graph, opinion, self.model_params)
     self.model = model
+    self.steps = 0
     self.init_data()
 
   def dump(self):
     # graph
     graph = nx.DiGraph(self.model.graph)
-    agents: List[HKAgent] = []
+    agents: List[HKAgent] = self.model.schedule.agents
     # opinion
     opinion = np.zeros((self.model.graph.number_of_nodes(), ), dtype=float)
     for a in agents:
@@ -67,9 +79,16 @@ class Scenario:
     # data
     c = self.datacollector
     data = (c.model_vars, c._agent_records, c.tables)
-    return graph, opinion, data
+    return graph, opinion, data, self.stats, self.steps
 
-  def load(self, graph: nx.DiGraph, opinion: Dict[int, float], data: Optional[Tuple[dict, dict, dict]] = None):
+  def load(
+      self, 
+      graph: nx.DiGraph, 
+      opinion: Dict[int, float], 
+      data: Optional[Tuple[dict, dict, dict]] = None,
+      stats: Dict[int, StatsType] = None,
+      step: int = 0,
+    ):
     self.model = HKModel(graph, opinion, self.model_params)
     if data is not None:
       self.init_data(collect=False)
@@ -79,14 +98,22 @@ class Scenario:
       self.datacollector.tables = t
     else:
       self.init_data()
+    if stats is not None:
+      self.stats = stats
+    self.steps = step or 0
       
   def step_once(self):
     self.model.step()
-    self.datacollector.collect(self.model)
+    self.steps += 1
+    
+    if self.steps % self.scenario_params.data_interval == 0:
+      self.datacollector.collect(self.model)
+    if self.steps % self.scenario_params.stat_interval == 0:
+      self.add_stats()
     
   def step(self, count: int = 0):
     if count < 1:
-      count = self.scenario_params.n_step
+      count = self.scenario_params.total_step
     for _ in tqdm(range(count)):
       self.step_once()
       
@@ -97,20 +124,34 @@ class Scenario:
     dr = data['DiffRecommended'].unstack().to_numpy()
     return opinion, dn, dr
   
-  def collect_stats(self):
-    graph, o_slice, _ = self.dump()
+  def add_stats(self):
+    self.stats[self.steps] = self.collect_stats()
+  
+  def collect_stats(self, hist_interval=0.05):
+    digraph, opinion, _, _, _ = self.dump()
+    graph = nx.Graph(digraph)
+    n = graph.number_of_nodes()
     
     # distance distribution
-    o_slice_mat = np.tile(o_slice.reshape((o_slice.size, 1)), o_slice.size)
-    o_slice_dist = np.abs(o_slice_mat - o_slice_mat.T)
+    o_slice_mat = np.tile(opinion.reshape((opinion.size, 1)), opinion.size)
+    o_sample = np.abs(o_slice_mat - o_slice_mat.T).flatten()
+    distance_dist = np.histogram(o_sample, bins=np.arange(-1, 1 + hist_interval, hist_interval))
     
     # closed triads' count
-    triads = nx.triangles(nx.Graph(graph))
-    
+    triads = nx.triangles(graph)
+    triads_count = sum(triads.values()) // 3
     
     # clustering coefficient
-
-
+    clustering = nx.average_clustering(graph)
 
     # segregation index
+    positive_amount = max(1, np.sum(opinion > 0))
+    negative_amount = max(1, n - positive_amount)
+    edge_interconnection = len([None for u, v in graph.edges if opinion[u] * opinion[v] <= 0])
+    
+    density = graph.number_of_edges() / (n * (n - 1) / 2)
+    s_index: float = 1 - edge_interconnection / \
+      (2 * density * positive_amount * negative_amount)
+      
+    return distance_dist, triads_count, clustering, s_index
     
