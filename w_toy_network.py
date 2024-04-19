2d2d4b8056b496cdafd4415261478a890195e604
Bill Stark <billstark001@gmail.com>
2024-04-19 23:05:09 +0900

    reform toy nw

--- w_toy_network.py
+++ w_toy_network.py
@@ -1,19 +1,43 @@
+from typing import cast, Tuple, Any, List
+
 import os
+
 import networkx as nx
 import numpy as np
+from scipy.interpolate import interp1d
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from matplotlib.axes import Axes
 import seaborn as sns
+
 from base import HKModelParams, Scenario, SimulationParams
 from env import RandomNetworkProvider, ScaleFreeNetworkProvider
 from recsys import Random, Opinion, Structure
 import stats
+from w_param_plot import area_under_curve
+from w_plot_utils import plt_figure
 
-raise Exception('do not run this file again')
+BASE_PATH = './fig_final'
+
+os.makedirs(BASE_PATH, exist_ok=True)
+mpl.rcParams['font.size'] = 18
+sns.set_theme(style='whitegrid')
+
+def plt_save_and_close(path: str, eps=True, png=True):
+  if eps:
+    plt.savefig(path + '.eps', dpi=300, bbox_inches='tight')
+  if png:
+    plt.savefig(path + '.png', dpi=300, bbox_inches='tight')
+  return plt.close()
 
 stat_collectors = {
-    # 'distance': stats.DistanceCollectorDiscrete(use_js_divergence=True),
-    'layout': stats.NetworkLayoutCollector()
+  'layout': stats.NetworkLayoutCollector(),
+  's-index': stats.SegregationIndexCollector(),
+  'distance': stats.DistanceCollectorDiscrete(
+      use_js_divergence=True,
+      hist_interval=0.08,
+  ),
 }
 
 s_params = RandomNetworkProvider(
@@ -21,72 +45,110 @@ s_params = RandomNetworkProvider(
     agent_follow=15,
 )
 sim_p_standard = SimulationParams(
-    max_total_step=114514,
-    stat_interval=114514,
+    max_total_step=1000,
+    stat_interval=15,
     stat_collectors=stat_collectors
 )
 
-params = HKModelParams(
+params1= HKModelParams(
     tolerance=0.4,
-    decay=0.04,
-    rewiring_rate=0.02,
+    decay=0.05,
+    rewiring_rate=0.01,
     recsys_count=10,
     recsys_factory=lambda m: Opinion(m),
 )
 
+params2 = HKModelParams(
+    tolerance=0.4,
+    decay=0.01,
+    rewiring_rate=0.05,
+    recsys_count=10,
+    recsys_factory=lambda m: Opinion(m),
+)
 
-BASE_PATH = './fig_final'
+# colormap for network plotting
 
-os.makedirs(BASE_PATH, exist_ok=True)
-
-def plt_save_and_close(path: str):
-  plt.savefig(path + '.eps', dpi=300, bbox_inches='tight')
-  plt.savefig(path + '.png', dpi=300, bbox_inches='tight')
-  return plt.close()
-
-S = Scenario(s_params, params, sim_p_standard)
-step = 300
-S.init()
-l0 = S.collect_stats()['layout']
-S.step(step)
-l1 = S.collect_stats()['layout']
-S.step(step)
-l2 = S.collect_stats()['layout']
-S.step(step // 2)
-
-x = 12
-width = x / 3
-height = width * 3 / 4
 cmap = 'coolwarm'
 
 norm = mpl.colors.Normalize(vmin=-1, vmax=1)
 sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
 sm.set_array([])
 
-mpl.rcParams['font.size'] = 18
+for i_p, params in enumerate((params1, params2)):
+    
+  S = Scenario(s_params, params, sim_p_standard)
+  S.init()
+  S.step()
 
-fig, axes = plt.subplots(1, 3, figsize=(x, height))
-for i, (pos, color, G) in enumerate([l0, l1, l2]):
-  ax = axes[i]
-  ax.spines['top'].set_visible(False)
-  ax.spines['bottom'].set_visible(False)
-  ax.spines['left'].set_visible(False)
-  ax.spines['right'].set_visible(False)
-  nx.draw_networkx_nodes(
-      G, ax=ax, pos=pos, node_color=color, cmap=cmap, vmin=-1, vmax=1, node_size=40)
-  nx.draw_networkx_edges(G, ax=ax, pos=pos, node_size=40, alpha=0.36)
-  ax.set_xlabel(f't = {i * step}')
+  # generate stats
 
-plt.colorbar(sm, ticks=np.linspace(-1, 1, 5), ax=axes[2])
-plt.subplots_adjust(wspace=0.1, hspace=0.1)
+  if S.steps not in S.stats:
+    S.add_stats()
+    
+  S_data_steps, opinion, dn, dr, sum_n, sum_r, n_n, n_r = S.get_opinion_data()
+  S_stats = S.generate_stats()
+  S_stat_steps = S_stats['step']
+  
+  fig, (axop, axst, axhp) = cast(Tuple[Any, List[Axes]], plt_figure(n_col=3, total_width=12))
+  
+  n_edges = np.array(sorted(list(S.model.graph.out_degree), key=lambda x: x[0]))[:, 1]
+  h_index = np.mean(n_n / n_edges[np.newaxis, :], axis=1)
+  if h_index.shape[0] > 1:
+    h_index[0] = h_index[1]
+  s_index = S_stats['s-index']
+  p_index = 1 - np.array(S_stats['distance-worst-o'])
+  g_index = 1 - np.array(S_stats['distance-worst-s'])
+  
+  # plot opinion data
+  
+  axop.plot(opinion, lw=0.5)
+  axop.set_title('(a) opinion evolution')
+  axop.set_xlabel('step')
+  
+  # plot indices
+  
+  axst.plot(S_data_steps, h_index, linewidth=1)
+  axst.plot(S_stat_steps, s_index, linewidth=1)
+  axst.plot(S_stat_steps, p_index, linewidth=1)
+  axst.plot(S_stat_steps, g_index, linewidth=1)
+  axst.legend(['homophily', 'segregation', 'polarization', 'general'])
+  
+  axst.set_title('(b) indices along simulation')
+  axst.set_xlabel(f'step')
+  axst.set_ylabel('index')
+  
+  # plot curves
+  
+  p_index_resmpl = interp1d(S_stat_steps, p_index, kind='linear')(S_data_steps)
+  gradation = area_under_curve([p_index_resmpl, h_index])
+  axhp.plot(p_index_resmpl, h_index)
+  axhp.set_ylabel('homophily')
+  axhp.set_xlabel('polarization')
+  axhp.set_title(f'(c) H-P curves (AUC: {gradation:.4f})')
 
-plt_save_and_close(os.path.join(BASE_PATH, 'toy_network'))
+
+  plt_save_and_close(os.path.join(BASE_PATH, f'toy_stats_{i_p}'))
+  
+  # plot 3 snapshots
+
+  fig, axes = cast(Tuple[Any, List[Axes]], plt_figure(n_col=3, hw_ratio=1, total_width=10))
+  layout = S_stats['layout']
+  for i, j in enumerate([0, 20, 40]):
+    (pos, color, G) = layout[j]
+    ax = axes[i]
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    nx.draw_networkx_nodes(
+        G, ax=ax, pos=pos, node_color=color, cmap=cmap, vmin=-1, vmax=1, node_size=40)
+    nx.draw_networkx_edges(G, ax=ax, pos=pos, node_size=40, alpha=0.36)
+    ax.set_xlabel(f'step = {S_stat_steps[j]}')
+
+  plt.colorbar(sm, ticks=np.linspace(-1, 1, 5), ax=axes[2])
+  plt.subplots_adjust(wspace=0.1, hspace=0.1)
+
+  plt.tight_layout()
+  plt_save_and_close(os.path.join(BASE_PATH, f'toy_network_{i_p}'), eps=False)
 
 
-sns.set_theme()
-steps, opinion, dn, dr = S.get_opinion_data()  # (t, n)
-plt.figure(figsize=(4, 3))
-plt.plot(opinion, lw=0.5)
-plt.title('Opinion')
-
-plt_save_and_close(os.path.join(BASE_PATH, 'toy_opinion'))
