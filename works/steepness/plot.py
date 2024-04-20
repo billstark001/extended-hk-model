7db22db8db66361e28ae3efbc3bbeb0e3a8ac835
Bill Stark <billstark001@gmail.com>
2024-04-21 05:42:52 +0900

    *

--- works/steepness/plot.py
+++ works/steepness/plot.py
@@ -1,12 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.stats import gaussian_kde
+
 # drafts
 
 
-data_ = [x for x in data[9::9] if x['p_last'] >= -0.6]
-plt.scatter([x['steepness']for x in data_], [x['active_step'] for x in data_], s=2)
-plt.yscale('log')
-plt.xscale('log')
-plt.grid(True)
-plt.show()
+# data_ = [x for x in data[9::9] if x['p_last'] >= -0.6]
+# plt.scatter([x['steepness']for x in data_], [x['active_step'] for x in data_], s=2)
+# plt.yscale('log')
+# plt.xscale('log')
+# plt.grid(True)
+# plt.show()
+
+data_ = [x for x in data[::9] if x['p_last'] >= 0.6]
+data_x = [x['steepness']for x in data_]
+data_y = [x['active_step'] for x in data_]
+
+X = np.array(data_x)
+X[X < 0.125] = 0.125
+Y = np.array(data_y)
+
+X = np.log10(X)
+Y = np.log10(Y)
+
+kde_X = gaussian_kde(X)
+kde_Y = gaussian_kde(Y)
+
+kde_XY = gaussian_kde(np.vstack([X, Y]))
+
+grid_resolution = 400
+x_grid = np.linspace(X.min(), X.max(), grid_resolution)
+y_grid = np.linspace(Y.min(), Y.max(), grid_resolution)
+X_grid, Y_grid = np.meshgrid(x_grid, y_grid)
+
+pdf_X = np.reshape(kde_X(x_grid), x_grid.shape)
+pdf_Y = np.reshape(kde_Y(y_grid), y_grid.shape)
+pdf_XY = np.reshape(kde_XY(np.vstack([X_grid.ravel(), Y_grid.ravel()])), X_grid.shape)
+
+mutual_info = np.sum(pdf_XY * np.log(pdf_XY / (pdf_X[:, None] * pdf_Y[None, :])))
+mutual_info
